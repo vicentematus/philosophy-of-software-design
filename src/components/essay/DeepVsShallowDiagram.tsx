6e1420d584cdd5c0c78b726136a8e03b4b3664c5
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2025-12-29 23:27:25 -0300

    feat(): address @facundoolano comment on shallow module diagram

--- src/components/essay/DeepVsShallowDiagram.tsx
+++ src/components/essay/DeepVsShallowDiagram.tsx
@@ -1,29 +1,26 @@
 export function DeepVsShallowDiagram() {
   return (
     <div className="diagram-container">
-      <div className="diagram-title">Deep vs Shallow Classes</div>
+      <div className="diagram-title">Deep vs Shallow Modules</div>
       <div className="diagram-row">
-        {/* Deep Class */}
-        <div className="class-box">
-          <div className="class-interface class-interface--small">
-            Small Interface
+        {/* Deep Module */}
+        <div className="module-box module-box--deep">
+          <div className="module-label-top">small interface</div>
+          <div className="module-interface" />
+          <div className="module-implementation module-implementation--tall">
+            <span>big<br />implementation</span>
           </div>
-          <div className="class-implementation class-implementation--large">
-            Large Implementation
-            <div className="hidden-complexity">(hides complexity)</div>
-          </div>
-          <div className="class-label class-label--good">Deep Class</div>
+          <div className="class-label class-label--good">Deep Modules</div>
         </div>
 
-        {/* Shallow Class */}
-        <div className="class-box">
-          <div className="class-interface class-interface--large">
-            Large Interface
+        {/* Shallow Module */}
+        <div className="module-box module-box--shallow">
+          <div className="module-label-top">big interface</div>
+          <div className="module-interface" />
+          <div className="module-implementation module-implementation--short">
+            <span>small implementation</span>
           </div>
-          <div className="class-implementation class-implementation--small">
-            Small Implementation
-          </div>
-          <div className="class-label class-label--bad">Shallow Class</div>
+          <div className="class-label class-label--bad">Shallow Modules</div>
         </div>
       </div>
     </div>
